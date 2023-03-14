a18d974a8cb1ddd5a9da219fc5005b595ef2ddbe
LauraBarrigaDI <113401273+LauraBarrigaDI@users.noreply.github.com>
2023-03-14 12:20:09 -0500

    Prueba5

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -30,18 +30,6 @@ galleryContainer.addEventListener('click', event => {
     modal.show();
   }
 });
-console.log(galleryItems);
-const gallery = document.querySelector('.gallery');
-
-const galleryHTML = galleryItems.map(({ preview, original, description }) => {
-  return `
-    <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" data-caption="${description}"/>
-    </a>
-  `;
-}).join('');
-
-gallery.insertAdjacentHTML('beforeend', galleryHTML);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captions: true,
