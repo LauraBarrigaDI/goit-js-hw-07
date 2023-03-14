42c0fdeb1625c7c2e2993e6363c67e73e350c2be
LauraBarrigaDI <laurabarriga31@gmail.com>
2023-03-13 21:32:25 -0500

    prueba_task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,9 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
