341418d4d48c29ad00eda6999c14ffb1789f9e57
LauraBarrigaDI <113401273+LauraBarrigaDI@users.noreply.github.com>
2023-03-14 12:17:12 -0500

    Update1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemTemplate = document.querySelector('#gallery-item-template');
+
+galleryItems.forEach(item => {
+  const galleryItem = galleryItemTemplate.content.cloneNode(true);
+  const galleryLink = galleryItem.querySelector('.gallery__link');
+  const galleryImage = galleryItem.querySelector('.gallery__image');
+
+  galleryLink.href = item.original;
+  galleryImage.src = item.preview;
+  galleryImage.dataset.source = item.original;
+  galleryImage.alt = item.description;
+
+  galleryContainer.appendChild(galleryItem);
+});
+
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+  
+  const galleryImage = event.target.closest('.gallery__image');
+  
+  if (galleryImage) {
+    const largeImageUrl = galleryImage.dataset.source;
+    
+    const modal = basicLightbox.create(`<img src="${largeImageUrl}">`);
+    modal.show();
+  }
+});
+console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 
 const galleryHTML = galleryItems.map(({ preview, original, description }) => {
